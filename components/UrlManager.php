8a27aa04c89cb440430d63cc0f7236f09d57f02c
mytreo <mytreogm@gmail.com>
2016-05-26 15:37:21 +0300

    filter completely work

--- components/UrlManager.php
+++ components/UrlManager.php
@@ -1,12 +1,10 @@
 <?php
 class UrlManager extends Component {
 	public function getDomain() {
-		#echo "http://".$_SERVER['HTTP_HOST'].substr($_SERVER['REQUEST_URI'],0,stripos ( $_SERVER['REQUEST_URI'], '.php')+4)."<br/>";
 		return "http://".$_SERVER['HTTP_HOST'].substr($_SERVER['REQUEST_URI'],0,stripos ( $_SERVER['REQUEST_URI'], '.php')+4);
 	}
 
 	public function getAddress($requestedPage=null){
-		echo $this->getDomain() . (!is_null($requestedPage)?"?r=".$requestedPage:null);
 		return $this->getDomain() . (!is_null($requestedPage)?"?r=".$requestedPage:null);
 	}
 }
--- views/mMenu.php
+++ views/mMenu.php
@@ -1,7 +1,8 @@
 <ul class ="menu">
-	<li><a href= <?php Application::getInstance()->urlManager->getAddress();  ?> >«домашняя страница»</a></li>
-	<li><a href=<?php Application::getInstance()->urlManager->getAddress('help/about');  ?> >«справка/о программе»</a></li>
-	<li><a href=<?php Application::getInstance()->urlManager->getAddress('help/manual');  ?>  >«справка/инструкция»</a></li>
-	<li><a href=<?php Application::getInstance()->urlManager->getAddress('info/company');  ?>  >«информация/о компании»</a></li>
-	<li><a href=<?php Application::getInstance()->urlManager->getAddress('info/terms');  ?>  >«информация/условия использования»</a></li>
+	<li><a href= <?php echo Application::getInstance()->urlManager->getAddress();  ?> >«домашняя страница»</a></li>
+	<li><a href=<?php  echo Application::getInstance()->urlManager->getAddress('help/about');  ?>>«справка/о программе»</a></li>
+	<li><a href=<?php echo Application::getInstance()->urlManager->getAddress('help/manual');  ?>>«справка/инструкция»</a></li>
+	<li><a href=<?php echo Application::getInstance()->urlManager->getAddress('info/company');  ?>  >«информация/о компании»</a></li>
+	<li><a href=<?php echo Application::getInstance()->urlManager->getAddress('info/company').'&checkValue=some'; ?>>I2</a></li>
+	<li><a href=<?php echo Application::getInstance()->urlManager->getAddress('info/terms');  ?>>«информация/условия использования»</a></li>
 </ul>
