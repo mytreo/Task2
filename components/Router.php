07f74829b8a08012d6700e2562f915dfaa8b241e
mytreo <mytreogm@gmail.com>
2016-05-26 09:27:29 +0300

    last route implementation

--- components/Router.php
+++ components/Router.php
@@ -17,31 +17,24 @@ class Router extends Component {
         $wayArray    = explode('/', $_GET["r"], 3);
         $wayArray[0] = ($wayArray[0] == null) ? 'default' : $wayArray[0]; //controller
         $wayArray[1]; //method
-        try {
-            $controllerClassName = 'controllers/' . $wayArray[0] . 'Controller' . '.php';
-            if (!class_exists($controllerClassName)) {
-                $controllerClassName = Autoloader::getInstance()->addClassPath($controllerClassName);
-            }
-
-            $routeName   = 'action'.$wayArray[1];
-            $routeParams = (!array_key_exists(2, $wayArray)) ? '' : $wayArray[2];
-            if (method_exists($controllerClassName, $routeName)) {
-                return call_user_func(array($controllerClassName, $routeName), $routeParams);
-            } else {
-                #throw new RoutingException("No Way in '" . get_class($controllerClassName) . "/{$routeName}'!");
-                return call_user_func(array('DefaultController', 'actionindex'), '');
-            }
-
-        } catch (RoutingException $ex) {
-            echo $ex->getMessage().' Redirect to Home';
-           # return call_user_func(array('DefaultController', 'actionindex'), '');
-            /* Redirect browser */
-            ##header("Location: http://localhost:63342/Task1/index.php");
-            #header("Location: http://". $_SERVER["HTTP_HOST"]."/Task1/index.php");
-           # exit();
-        }
+        $routeParams = (!array_key_exists(2, $wayArray)) ? '' : $wayArray[2];
+        return $this->go($wayArray[0],$wayArray[1],$routeParams);
+      }
+        $this->go('default', 'index','');
     }
 
+    public function go($controllerName, $controllerAction, $controllerActionParams){
+        $controllerClassName = 'controllers/' . $controllerName . 'Controller' . '.php';
+        if (!class_exists($controllerClassName)) {
+            $controllerClassName = Autoloader::getInstance()->addClassPath($controllerClassName);
+        }
+        $routeName   = 'action'.$controllerAction;
+        $controllerInstanse= new $controllerClassName();
+        if (method_exists($controllerClassName, $routeName)) {
+            return call_user_func(array($controllerInstanse, $routeName), $controllerActionParams);
+        } else {
+            return $this->go('default', 'index','');
+        }
     }
     
     public function setDefaultControllerName($controllerName) {
